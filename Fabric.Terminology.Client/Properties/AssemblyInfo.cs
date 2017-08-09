c626c333d5995211dc601fcf1d3bcd010caaf333
Rusty Swayne <rusty.swayne@healthcatalyst.com>
2017-08-09 13:02:16 -0700

    Build versioning

---
+++ Fabric.Terminology.Client/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Reflection;
+
+[assembly: CLSCompliant(true)]
+
+[assembly: AssemblyTitle("Fabric.Terminology")]
+[assembly: AssemblyDescription("Service to provide shared healthcare terminology data")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Health Catalyst")]
+[assembly: AssemblyProduct("Fabric.Terminology")]
+[assembly: AssemblyCopyright("Copyright 2017 © Health Catalyst")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("0.4.1.0")]
+[assembly: AssemblyFileVersion("0.4.1.0")]
