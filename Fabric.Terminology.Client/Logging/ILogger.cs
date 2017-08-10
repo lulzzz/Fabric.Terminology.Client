417f8a9497539980d16f7b71e389f7eedd5a1fd4
Rusty Swayne <rusty.swayne@healthcatalyst.com>
2017-08-10 10:40:56 -0700

    Implements method stubs.

---
+++ Fabric.Terminology.Client/Logging/ILogger.cs
@@ -0,0 +1,27 @@
+﻿namespace Fabric.Terminology.Client.Logging
+{
+    using System;
+
+    public interface ILogger
+    {
+        void Error(Type callingType, string message, Exception exception, params Func<object>[] propertyValues);
+
+        void Error(string message, Exception exception, params Func<object>[] propertyValues);
+
+        void Warning(Type callingType, string message, Exception exception, params Func<object>[] propertyValues);
+
+        void Warning(string message, Exception exception, params Func<object>[] propertyValues);
+
+        void Information(string message);
+
+        void Information(string message, params Func<object>[] propertyValues);
+
+        void Information(Type callingType, string message, params Func<object>[] propertyValues);
+
+        void Debug(string message);
+
+        void Debug(string message, params Func<object>[] propertyValues);
+
+        void Debug(Type callingType, string message, params Func<object>[] propertyValues);
+    }
+}
