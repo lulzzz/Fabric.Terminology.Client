62c3f119258dde42588b3e03f13847880e2d77a2
Rusty Swayne <rusty.swayne@healthcatalyst.com>
2017-08-10 15:42:42 -0700

    Extends TerminolgoyApiSettings to include ValueSetMeta

--- Fabric.Terminology.Client/Configuration/ITerminologyApiSettings.cs
+++ Fabric.Terminology.Client/Configuration/ITerminologyApiSettings.cs
@@ -1,9 +1,13 @@
 ﻿namespace Fabric.Terminology.Client.Configuration
 {
+    using Fabric.Terminology.Client.Models;
+
     public interface ITerminologyApiSettings
     {
         string TerminologyApiUri { get; set; }
 
         string ApiVersion { get; set; }
+
+        ValueSetMeta ValueSetMeta { get; set; }
     }
 }
