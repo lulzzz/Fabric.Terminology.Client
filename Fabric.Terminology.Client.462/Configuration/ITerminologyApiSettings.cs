c001daaa195062a99f62916fdbd7c8a1bbed9a3a
Rusty Swayne <rusty.swayne@healthcatalyst.com>
2017-08-11 15:34:22 -0700

    Back ports Fabric.Terminology.Client project to v462 and links to new .netstandard project to support multiple NuGet versions.

---
+++ Fabric.Terminology.Client.462/Configuration/ITerminologyApiSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace Fabric.Terminology.Client.Configuration
+{
+    using Fabric.Terminology.Client.Models;
+
+    public interface ITerminologyApiSettings
+    {
+        string TerminologyApiUri { get; set; }
+
+        string ApiVersion { get; set; }
+
+        ValueSetMeta ValueSetMeta { get; set; }
+    }
+}
