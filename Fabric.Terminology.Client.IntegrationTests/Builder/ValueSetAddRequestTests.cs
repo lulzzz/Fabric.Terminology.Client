c26075b7d5eb79b998cbd04950806be12a7c3aa4
Rusty Swayne <rusty.swayne@healthcatalyst.com>
2017-10-09 15:29:56 -0700

    Patches test.

--- Fabric.Terminology.Client.IntegrationTests/Builder/ValueSetAddRequestTests.cs
+++ Fabric.Terminology.Client.IntegrationTests/Builder/ValueSetAddRequestTests.cs
@@ -1,5 +1,10 @@
-﻿namespace Fabric.Terminology.Client.IntegrationTests.Builder
+﻿using System.Linq;
+using Fabric.Terminology.Client.Models;
+
+namespace Fabric.Terminology.Client.IntegrationTests.Builder
 {
+    using System;
+    using System.Collections.Generic;
     using Fabric.Terminology.Client.TestsBase.Fixtures;
     using Fabric.Terminology.Client.TestsBase.Mocks;
     using FluentAssertions;
@@ -13,26 +18,39 @@
         {
         }
 
-        //[Theory(Skip = "Manual run only. Delete not implemented.")]
-        ////[Theory]
-        //[InlineData("Api.ValueSet1", 10)]
-        //[InlineData("Api.ValueSet2", 5)]
-        //[InlineData("Api.ValueSet3", 2500)]
-        //public void CanAddAValueSet(string name, int codeCount)
-        //{
-        //    // Arrange
-        //    var codes = MockApiModelBuilder.BuildCodeSetCodeCollection(codeCount);
+        [Theory(Skip = "Manual run only. Delete not implemented.")]
+        //[Theory]
+        [InlineData("Api.ValueSet1", 10)]
+        [InlineData("Api.ValueSet2", 5)]
+        [InlineData("Api.ValueSet3", 2500)]
+        public void CanAddAValueSet(string name, int codeCount)
+        {
+            // Arrange
+            var codeSystemGuids = new List<Guid>
+                {
+                    Guid.NewGuid(),
+                    Guid.NewGuid()
+                };
+            var codes = MockApiModelBuilder.BuildCodeSetCodeCollection(codeSystemGuids, codeCount);
 
-        //    // Act
-        //    var query = this.SharedTerminology.ValueSets.Add(name, codes);
-        //    var maybe = this.Profiler.ExecuteTimed(async () => await query.Execute());
+            // Act
+            var meta = new ValueSetMeta
+            {
+                AuthoringSourceDescription = "Fabric.Terminology.Client Test",
+                DefinitionDescription = "Integration test",
+                SourceDescription = "Testing client API",
+                VersionDate = DateTime.UtcNow
+            };
 
-        //    // Assert
-        //    maybe.HasValue.Should().BeTrue();
-        //    var valueSet = maybe.Single();
+            var query = this.TerminologyContext.ValueSets.Add(name, meta, codes);
+            var maybe = this.Profiler.ExecuteTimed(async () => await query.Execute());
 
-        //    valueSet.IsCustom.Should().BeTrue();
-        //    valueSet.ValueSetCodesCount.Should().Be(codeCount);
-        //}
+            // Assert
+            maybe.HasValue.Should().BeTrue();
+            var valueSet = maybe.Single();
+
+            valueSet.IsCustom.Should().BeTrue();
+            valueSet.CodeCounts.Sum(x => x.CodeCount).Should().Be(codeCount);
+        }
     }
 }
